0b7045fb7db8dec6bc94c1f9a240a6ba4dd6dea1
ivan <kulikovia.72@icloud.com>
2022-10-24 19:04:40 +0300

    simplify manifest

--- Package.swift
+++ Package.swift
@@ -4,9 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Builder",
-    platforms: [
-        .iOS(.v16)
-    ],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "Builder", targets: ["Builder"])
     ],
